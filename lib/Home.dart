ac24dae0d3a7cde0538de1c465a5f493810f486d
Peterson <pepe419@gmail.com>
2021-06-10 08:30:16 -0300

    Finalizado a estrutura da tela de home

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,10 +1,41 @@
+import 'package:cara_ou_coroa/TelaResultado.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
 
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  _abrirTelaResultado() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => TelaResultado(),
+        ));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green,);
+    return Container(
+      color: Color(0xff61bd86),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('images/logo.png'),
+          Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: GestureDetector(
+              onTap: () {
+                _abrirTelaResultado();
+              },
+              child: Image.asset('images/botao_jogar.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/TelaResultado.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaResultado extends StatefulWidget {
+  const TelaResultado({Key key}) : super(key: key);
+
+  @override
+  _TelaResultadoState createState() => _TelaResultadoState();
+}
+
+class _TelaResultadoState extends State<TelaResultado> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
